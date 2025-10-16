f9f306cc7e8fc89ae0fd5688db1a995fcea649ce
Vicente <vicente.oliva.p@ug.uchile.cl>
2025-10-16 15:20:31 -0300

    Creado cliente.py

---
+++ src/client.py
@@ -0,0 +1,40 @@
+import socket
+import sys
+
+def main():
+    # Borra esto despues waton, acá va la confi del socket UDP
+    HOST = 'localhost'
+    PORT = 5000
+    CHUNK_SIZE = 16
+    
+    # BORRA DSP BENJA acá se crea el socket UDP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    try:
+        # AYYY BENJAAAAA lee la ruta del archivoooo pero desde la entrada estándar
+        file_path = input("Ingrese la ruta del archivo a enviar: ")
+        
+        # Abrir y leer el archivo waton, no se si le sabes
+        
+        with open(file_path, 'rb') as file:
+            while True:
+                # Maximo 16 bytes, no se aceptan watones
+                chunk = file.read(CHUNK_SIZE)
+                
+                # si no quedan gatitas salimos
+                if not chunk:
+                    break
+                # Enviamos la data al sv cabron
+                client_socket.sendto(chunk, (HOST, PORT))
+                print(f"Enviado: {chunk}")
+            # No te voy a mentir el debug se autocompleto
+        print("Archivo enviado correctamente.")
+    except FileNotFoundError:
+        print("Error: Archivo no encontrado.")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        client_socket.close()
+
+if __name__ == "__main__":
+    main()
